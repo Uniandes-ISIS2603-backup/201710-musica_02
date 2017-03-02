705efc8abb386595840bf5c929a8d0faacfb032b
p.salazar12 <p.salazar12@ISIS2603-0144.sis.virtual.uniandes.edu.co>
2017-03-02 12:18:48 -0500

    Listo :)

---
+++ musica-logic/src/main/java/co/edu/uniandes/csw/musica/ejbs/ArtistaLogic.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.musica.ejbs;
+
+/**
+ *
+ * @author p.salazar12
+ */
+public class ArtistaLogic {
+    
+}
