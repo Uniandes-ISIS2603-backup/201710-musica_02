948ed6f74a6e6e1fe8b3803554f927bcb65e6b84
jd.gonzaleza <jd.gonzaleza@ISIS2603-0150.sis.virtual.uniandes.edu.co>
2017-03-02 12:24:04 -0500

    funcion

---
+++ musica-logic/src/main/java/co/edu/uniandes/csw/musica/ejbs/FuncionLogic.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.musica.ejbs;
+
+import java.util.Date;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author jd.gonzaleza
+ */
+@Stateless
+public class FuncionLogic {
+    private Date fecha;
+    
+}
