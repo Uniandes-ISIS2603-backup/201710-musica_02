bda99a2c8dc757aa030dba8e95007e4457c82024
af.olivares10 <af.olivares10@ISIS2603-0170.sis.virtual.uniandes.edu.co>
2017-03-09 17:39:44 -0500

    .

---
+++ musica-02-logic/src/main/java/co/edu/uniandes/csw/musica/ejbs/FestivalLogic.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.musica.ejbs;
+
+/**
+ *
+ * @author af.olivares10
+ */
+import co.edu.uniandes.csw.musica.entities.FestivalEntity;
+import co.edu.uniandes.csw.musica.exceptions.BusinessLogicException;
+import co.edu.uniandes.csw.musica.persistence.FestivalPersistence;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+@Stateless
+public class FestivalLogic {
+    
+    @Inject FestivalPersistence persistence;
+    
+    public List<FestivalEntity> getFestivales ()
+    {
+        return persistence.findAll();
+    }
+    public FestivalEntity getFestival (Long id) throws BusinessLogicException
+    {
+        FestivalEntity entity = persistence.find(id);
+        if(entity != null)
+        return persistence.find(id);
+        else throw new BusinessLogicException ("No se ha encontrado un festival con dicho Id");
+    }
+    public FestivalEntity createFestival (FestivalEntity festival) throws BusinessLogicException
+    {
+        FestivalEntity entity = persistence.find(festival.getId());
+        if(entity == null)
+        return persistence.create(festival);
+        // Puede que nunca la lance pues el Id es autogenerado
+        else throw new BusinessLogicException ("Ya existe un festival con ese Id");
+    }
+    
+}
---
+++ musica-02-logic/src/main/java/co/edu/uniandes/csw/musica/entities/FestivalEntity.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.musica.entities;
+
+/**
+ *
+ * @author af.olivares10
+ */
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+
+
+@Entity
+public class FestivalEntity implements Serializable
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date fechaInicio;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date fechaFin;
+   
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the fechaInicio
+     */
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    /**
+     * @param fechaInicio the fechaInicio to set
+     */
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    /**
+     * @return the fechaFin
+     */
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    /**
+     * @param fechaFin the fechaFin to set
+     */
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+    
+    
+}
