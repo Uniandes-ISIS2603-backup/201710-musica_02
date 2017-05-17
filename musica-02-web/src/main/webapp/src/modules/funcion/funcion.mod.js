4dfc763c6f0a6b50b6e23ac089eb5345a1c71915
jd.gonzaleza <jd.gonzaleza@ISIS2603-0150.sis.virtual.uniandes.edu.co>
2017-05-16 20:44:39 -0500

    Push y put más o menos

--- musica-02-web/src/main/webapp/src/modules/funcion/funcion.mod.js
+++ musica-02-web/src/main/webapp/src/modules/funcion/funcion.mod.js
@@ -82,33 +82,59 @@
                 param: {
                     funcionId: null
                 },
-               
 
                 views: {
 
                     detailView: {
-                        
- resolve: {
-                    currentFuncion: ['$http', 'funcionesContext', '$stateParams', function ($http, funcionesContext, $params) {
-                            return $http.get(funcionesContext + '/' + $params.funcionId);
-                        }]
-                },
+
+                        resolve: {
+                            currentFuncion: ['$http', 'funcionesContext', '$stateParams', function ($http, funcionesContext, $params) {
+                                    return $http.get(funcionesContext + '/' + $params.funcionId);
+                                }]
+                        },
                         templateUrl: basePath + 'funcion.detail.html',
                         controller: ['$scope', 'currentFuncion', '$stateParams', 'funcionContext', '$http', function ($scope, currentFuncion, $http, $params) {
 
                                 $scope.currentFuncion = currentFuncion.data;
                                 $scope.artistasRecords = currentFuncion.data.artistasDTOs;
                                 $scope.deleteFunciones = function () {
-                                   $http.delete("api/funciones/"+ currentFuncion.data.id);
-                                   $params.go('funcionList',{},{reload:true});
+                                    $http.delete("api/funciones/" + currentFuncion.data.id);
+                                    $params.go('funcionList');
                                 };
                             }]
                     },
                     listView: {
-                        templateUrl: basePathArtistas + 'artistas.list.html',
+                        templateUrl: basePathArtistas + 'artistas.list.html'
 
                     }
                 }
+            }).state('funcionUpdate',{
+                url:'/{funcionId:int}/actualizar',
+                param:{
+                    funcionId:null
+                },
+                parent:'funciones',
+                views:{
+                    updateView:{
+                        templateUrl: basePath +'funcion.update.html',
+                        resolve:{
+                            currentFuncion:['$http','funcionContext','$stateParams',function($http,funcionContext,$params){
+                                    return $http.get(funcionContext + '/'+ $params.funcionId);
+                            }]
+                        },
+                        controller:['scope','$http','currentFuncion','$state',function($scope,$http,currentFuncion,$state){
+                                $scope.funcion={};
+                                $scope.funcion.id = currentFuncion.data.id;
+                                $scope.putFuncion =function(){
+                                    $http.put("api/funciones/", $scope.funcion);
+                                    $state.reload();
+                                    $state.go('funcionList');
+                                };
+                                $scope.currentFuncion = currentFuncion.data;
+                        }]
+                    }
+                }
+                        
             });
         }
     ]);
